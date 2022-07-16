62ce09b56697d010423b61d8aa05e53ecb5cfb30
Vladimir Martsul <vladimir.martsul@alphawave.ru>
2022-07-17 01:33:57 +0300

    Register webhook after deploy

---
+++ public/setup.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+require dirname(__DIR__).'/vendor/autoload.php';
+
+use SergiX44\Nutgram\Nutgram;
+use Symfony\Component\Dotenv\Dotenv;
+
+use function Sentry\init;
+
+$dotenv = new Dotenv();
+$dotenv->loadEnv(dirname(__DIR__).'/.env');
+
+init(['dsn' => $_ENV['SENTRY_DSN']]);
+
+$bot = new Nutgram($_ENV['BOT_TOKEN']);
+$result = $bot->setWebhook($_ENV['APP_URL'], ['secret_token' => $_ENV['BOT_SECRET']]);
+
+exit($result);
