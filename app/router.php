c7e0c08eef2503334b5ca54d20f92ac6d835c70f
Vladimir Martsul <vladimir.martsul@gmail.com>
2022-08-10 15:10:32 +0300

    Remove redirect from dev URL

--- app/router.php
+++ app/router.php
@@ -27,12 +27,5 @@ if (str_ends_with((string)$_ENV['APP_URL'], '://'.$_SERVER['HTTP_HOST'])) {
     return true;
 }
 
-// Redirect requests from fly dev server to canonical address
-if ($_SERVER['HTTP_HOST'] === $_ENV['FLY_APP'].'.'.$_ENV['FLY_DOMAIN']) {
-    header('Location: '.$_ENV['APP_URL'].$_SERVER['REQUEST_URI'], true, 301);
-
-    return true;
-}
-
 // Reject other requests
 http_response_code(404);
