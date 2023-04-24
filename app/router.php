184ece41e3d8e4f51cfb71eab40f9eebcfc2122d
Vladimir Martsul <vladimir.martsul@gmail.com>
2023-04-24 14:25:45 +0300

    Offload only TLS on platform level

--- app/router.php
+++ app/router.php
@@ -12,7 +12,7 @@ $dotenv->loadEnv(dirname(__DIR__).'/.env');
 // Process requests from own canonical address
 if (str_ends_with((string) $_ENV['APP_URL'], '://'.$_SERVER['HTTP_HOST'])) {
     // Process normal requests
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['SCRIPT_NAME'] === '/index.php') {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['SCRIPT_NAME'] === '/index.php' && $_SERVER['REQUEST_URI'] === '/') {
         return false;
     }
 
@@ -24,6 +24,11 @@ if (str_ends_with((string) $_ENV['APP_URL'], '://'.$_SERVER['HTTP_HOST'])) {
     // Reject other requests
     http_response_code(405);
 
+    return true;
+}
+
+// Internal Consul Health Check
+if ($_SERVER['HTTP_USER_AGENT'] === 'Consul Health Check' && $_SERVER['REQUEST_METHOD'] === 'GET' && $_SERVER['SCRIPT_NAME'] === '/index.php' && $_SERVER['REQUEST_URI'] === '/') {
     return true;
 }
 
